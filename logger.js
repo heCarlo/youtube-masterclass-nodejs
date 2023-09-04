19ac11deb5dd853099cfac623672fcb6a1e5c84f
heCarlo <carlosgfkp@outlook.com>
2023-09-04 19:07:23 -0300

    att

--- logger.js
+++ logger.js
@@ -1,18 +1,24 @@
-const EventEmitter = require('events')
-const fs = require('fs')
-const path = require('path')
+// Importa os módulos necessários
+const EventEmitter = require('events');
+const fs = require('fs');
+const path = require('path');
 
-const emitter = new EventEmitter()
+// Cria uma instância de EventEmitter para lidar com eventos personalizados
+const emitter = new EventEmitter();
 
+// Define um ouvinte de eventos para o evento 'log', que recebe uma mensagem e a anexa a um arquivo de log
 emitter.on('log', (message) => {
+    // Usa o módulo 'fs' para adicionar a mensagem ao arquivo de log 'log.txt'
     fs.appendFile(path.join(__dirname, 'log.txt'), message, err => {
-        if (err) throw err
-    })
-})
+        if (err) throw err;
+    });
+});
 
-
-function log(message){
-    emitter.emit('log', message)
+// Função 'log' que emite o evento 'log' com a mensagem a ser registrada no arquivo de log
+function log(message) {
+    // Emite o evento 'log' com a mensagem especificada
+    emitter.emit('log', message);
 }
 
-module.exports = log
+// Exporta a função 'log' para que ela possa ser usada em outros módulos
+module.exports = log;
